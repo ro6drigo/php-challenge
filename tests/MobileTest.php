634319488dcda37e3671cff884f048ee701c096e
Rodrigo Chirinos <ro6drigo@gmail.com>
2022-07-14 17:19:24 -0500

    Test makeCallByName with valid Contact

--- tests/MobileTest.php
+++ tests/MobileTest.php
@@ -2,21 +2,37 @@
 
 namespace Tests;
 
-use Mockery as m;
 use PHPUnit\Framework\TestCase;
 
+use App\Call;
 use App\Mobile;
 use App\Carriers\FirstCarrier;
+use App\Interfaces\CarrierInterface;
 
 class MobileTest extends TestCase
 {
+	private CarrierInterface $provider;
+	private Mobile $mobile;
+
+	public function setUp(): void
+	{
+		parent::setUp();
+
+		$this->provider = $this->getMockBuilder(FirstCarrier::class)->getMock();
+		$this->mobile = new Mobile($this->provider);
+	}
+
 	/** @test */
 	public function it_returns_null_when_name_empty()
 	{
-		$provider = new FirstCarrier();
+		$this->assertNull($this->mobile->makeCallByName(''));
+	}
 
-		$mobile = new Mobile($provider);
+	/** @test */
+	public function it_returns_call_object_when_name_valid()
+	{
+		$this->provider->expects($this->once())->method('makeCall')->willReturn(new Call());
 
-		$this->assertNull($mobile->makeCallByName(''));
+		$this->assertInstanceOf(Call::class, $this->mobile->makeCallByName('Rodrigo'));
 	}
 }
